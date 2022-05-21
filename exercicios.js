9c7c479de39dae1f0ed535ab08322d222aac61f4
Keven Rodrigo dos Santos Escovedo <49500205+kevenescovedo@users.noreply.github.com>
2022-05-21 09:18:30 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,28 @@
+
+
+
+//1)Um cientista tem  3 recipientes, sendo eles o recipiente A, que contém o luiquido A
+//o recipiente B que contém liquido B, e o recipiente C que contém o luquido C,Mas 
+// o cientista deseja colocar no recipiente A, o liquido B, no recipiente B o liquido C
+//e no recipiente C o liquido A, ajude o cientista a resolver esse problema 
+
+//DICA PULO DO GATO: utilizar um recipiente auxiliar
+
+//2) Faça um programa que exiba 100 vezes que diga você é uma pessoa incrivel(sim você mesmo)
+// isto faz bem!!!!!!!!!
+
+//3) crie um pequeno programa no navegador que calcule o indíce de massa corpório e apresente se 
+//a pessoa está acima do pesso
+
+
+
+//4)Crie um função que percorra o vetor e mostre todos os números pares desse vetor(você pode utilizar um vetor fixo,ou seja definido)[]
+
+
+//5)Crie uma classe Jogador que contenha os seguintes atributos, time, gols, cartoes_vermelhos,
+//assistencias, jogos, media_gol_jogo(esse atributo é o resultado da divisão de gols/ jogos), e tenha
+// os metódos fazerGol, darAsistencia, que devem incrementar os  atributos
+
+
+
+
